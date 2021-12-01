775b386efce73f34bc3f19d530a866b1a140573a
Dave Briccetti <daveb@davebsoft.com>
2021-12-01 14:25:45 -0800

    Day 1

---
+++ day1.py
@@ -0,0 +1,25 @@
+import pathlib
+from itertools import pairwise
+from typing import Iterator
+
+def part1() -> int:
+    return num_increases(get_numbers())
+
+def part2() -> int:
+    return num_increases(sums_of_3())
+
+def num_increases(numbers: Iterator[int]) -> int:
+    'Return the number of times the values in numbers increases'
+    increases = (b > a for a, b in pairwise(numbers))
+    return sum(increases)
+
+def sums_of_3() -> Iterator[int]:
+    'Return the number of times the sum of three values increases'
+    numbers = list(get_numbers())
+    return (sum(numbers[i-2:i+1]) for i in range(2, len(numbers)))
+
+def get_numbers() -> Iterator[int]:
+    lines = pathlib.Path('input.txt').read_text().rstrip().split('\n')
+    return (int(line) for line in lines)
+
+print(part2())
