4b3f29c37521d5987e8f878f273707267e627b69
Dave Briccetti <daveb@davebsoft.com>
2021-12-04 20:15:44 -0800

    Improve code

--- src/4.py
+++ src/4.py
@@ -1,54 +1,47 @@
 from pathlib import Path
 from typing import List
 
-groups = Path('../data/4.txt').read_text().rstrip().split('\n\n')
-picks: List[int] = list(map(int, groups[0].split(',')))
+Card = List[List[int]]
 
-def get_board(groups: str) -> List[List[int]]:
-    board = [list(map(int, line.split())) for line in groups.split('\n')]
-    return board
+def get_card(groups: str) -> Card:
+    return [list(map(int, line.split())) for line in groups.split('\n')]
 
-boards = [get_board(groups[i]) for i in range(1, len(groups))]
-print(f'There are {len(boards)} boards')
-selecteds = [[[False for r in range(5)] for c in range(5)] for b in range(len(boards))]
-
-winner_found = False
-
-
-def is_winner(selected) -> bool:
-    if any(sum(selected[row]) == 5 for row in range(5)):
-        return True
-    for col in range(5):
-        if sum(selected[row][col] for row in range(5)) == 5:
+def update_card(card: Card, selected: Card, pick: int) -> tuple[bool, bool]:
+    def is_winner() -> bool:
+        if any(sum(selected[row]) == 5 for row in range(5)):
             return True
-    return False
+        for col in range(5):
+            if sum(selected[row][col] for row in range(5)) == 5:
+                return True
+        return False
 
-def update_board(board, selected, pick: int) -> tuple[bool, bool]:
     for ri in range(5):
         for ci in range(5):
-            if board[ri][ci] == pick:
+            if card[ri][ci] == pick:
                 selected[ri][ci] = True
-                return True, is_winner(selected)
+                return True, is_winner()
     return False, False
 
-def unmarked_numbers_sum(board, selected) -> int:
-    return sum([board[ri][ci] for ri in range(5) for ci in range(5) if not selected[ri][ci]])
+def unmarked_numbers_sum(card: Card, selected: Card) -> int:
+    return sum([card[ri][ci] for ri in range(5) for ci in range(5) if not selected[ri][ci]])
 
-num_winners = 0
+input_data_groups: list[str] = Path('../data/4.txt').read_text().rstrip().split('\n\n')
+picks: List[int] = list(map(int, input_data_groups[0].split(',')))
+cards = [get_card(input_data_groups[i]) for i in range(1, len(input_data_groups))]
+print(f'There are {len(cards)} cards')
+hits = [[[False for r in range(5)] for c in range(5)] for b in range(len(cards))]
+winner_found = False
 
-while picks and num_winners < 10:
+while picks and not winner_found:
     pick = picks.pop(0)
     print(f'{pick=}')
     num_updated = 0
-    for i, (board, selected) in enumerate(zip(boards, selecteds)):
-        updated, winner = update_board(board, selected, pick)
+    for i, (card, selected) in enumerate(zip(cards, hits)):
+        updated, winner = update_card(card, selected, pick)
         num_updated += updated
-        uns = unmarked_numbers_sum(board, selected)
         if winner:
-            num_winners += 1
-            print(f'Winner: {i=}, {uns=}, {uns * pick=}')
-        # print(board)
-        # print(selected)
+            winner_found = True
+            unmarked_sum = unmarked_numbers_sum(card, selected)
+            print(f'Winner: {i=}, {unmarked_sum=}, {unmarked_sum * pick=}')
     if num_updated:
         print(f'{num_updated=}')
-# print(selecteds)
