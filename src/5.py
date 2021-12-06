fcf98b5ebee6590fc96a15ca8271635d15abe394
Dave Briccetti <daveb@davebsoft.com>
2021-12-05 17:54:39 -0800

    Day 5 part 1 is working, part 2 is close

--- src/5.py
+++ src/5.py
@@ -1,9 +1,13 @@
+from collections import Counter
+from dataclasses import dataclass
 from pathlib import Path
+from itertools import chain
+from typing import Iterator
 
+@dataclass(frozen=True)
 class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+    x: int
+    y: int
 
 class Line:
     repr: str
@@ -15,7 +19,9 @@ class Line:
         '''
         self.repr = line_string
         pairs: list[str] = line_string.split(' -> ')
-        points = [Point(int(x), int(y)) for x, y in (map(int, pair.split(',')) for pair in pairs)]
+        points = [Point(int(x), int(y))
+                  for x, y in (map(int, pair.split(','))
+                               for pair in pairs)]
         self.points = points
 
     def __repr__(self):
@@ -30,36 +36,35 @@ class Line:
     def is_horz_or_vert(self) -> bool:
         return self.is_horz() or self.is_vert()
 
-    def contains_point(self, point: Point) -> bool:
+    def points_on_line(self) -> list[Point]:
+        xes = [p.x for p in self.points]
+        ys = [p.y for p in self.points]
+        min_x = min(xes)
+        max_x = max(xes)
+        min_y = min(ys)
+        max_y = max(ys)
         if self.is_horz():
-            min_x = min(p.x for p in self.points)
-            max_x = max(p.x for p in self.points)
-            return min_x <= point.x <= max_x
-        else:
-            min_y = min(p.y for p in self.points)
-            max_y = max(p.y for p in self.points)
-            return min_y <= point.y <= max_y
+            return [Point(x, self.points[0].y) for x in range(min_x, max_x + 1)]
+        if self.is_vert():
+            return [Point(self.points[0].x, y) for y in range(min_y, max_y + 1)]
+        dist = max_x - min_x
+        points = [Point(min_x + offset, min_y + offset) for offset in range(dist + 1)]
+        return points
 
 input_lines: list[str] = Path('../data/5_test.txt').read_text().rstrip().split('\n')
 lines: list[Line] = [Line(il) for il in input_lines]
-hv_lines: list[Line] = [line for line in lines if line.is_horz_or_vert()]
-max_x1 = max(line.points[0].x for line in hv_lines)
-max_x2 = max(line.points[1].x for line in hv_lines)
-max_x = max(max_x1, max_x2)
-max_y1 = max(line.points[0].y for line in hv_lines)
-max_y2 = max(line.points[1].y for line in hv_lines)
-max_y = max(max_y1, max_y2)
-
+horz_vert_lines: list[Line] = [line for line in lines if line.is_horz_or_vert()]
 
 def part1():
-    num_2_or_more = 0
-    for y in range(0, max_y + 1):
-        for x in range(0, max_x + 1):
-            if num_at_point := sum(line.contains_point(Point(x, y)) for line in hv_lines):
-                # print(f'({x}, {y}): {num_at_point}')
-                if num_at_point >= 2:
-                    num_2_or_more += 1
+    return num_with_at_least_2(horz_vert_lines)
 
-    return num_2_or_more
+def part2():
+    return num_with_at_least_2(lines)
 
-print(part1())
+def num_with_at_least_2(lines: Iterator[Line]) -> int:
+    points: list[Point] = list(chain(*(line.points_on_line() for line in lines)))
+    counter = Counter(points)
+    return sum(1 for num_lines_here in counter.values() if num_lines_here >= 2)
+
+
+print(part2())
