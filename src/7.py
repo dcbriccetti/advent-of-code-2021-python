58254010932a0dda0521131e42ce44eff48bb6dc
Dave Briccetti <daveb@davebsoft.com>
2021-12-07 15:06:59 -0800

    Improve code

--- src/7.py
+++ src/7.py
@@ -3,20 +3,20 @@ from pathlib import Path
 horz_positions: list[int] = list(map(int,
     Path('../data/7.txt').read_text().rstrip().split(',')))
 
-def triangle(n: int):
+def linear_cost(n: int):
+    return n
+
+def triangular_cost(n: int):
     return (n * (n + 1)) // 2
 
-def cost1(pos: int):
-    return sum(abs(pos - hp) for hp in horz_positions)
-
-def cost2(pos: int):
-    return sum(triangle(abs(pos - hp)) for hp in horz_positions)
+def cost(pos: int, fn):
+    return sum(fn(abs(pos - hp)) for hp in horz_positions)
 
 def solve():
     horz_positions.sort()
     median = horz_positions[len(horz_positions) // 2]
     print(f'{median=}, mean={sum(horz_positions) // len(horz_positions)}')
-    print(f'Part 1: ({cost1(median)}, {median})')
-    print('Part 2:', min((cost2(n), n) for n in range(min(horz_positions), max(horz_positions))))
+    print(f'Part 1: ({cost(median, triangular_cost)}, {median})')
+    print(f'Part 2: {min((cost(n, triangular_cost), n) for n in range(min(horz_positions), max(horz_positions)))}')
 
 print(solve())
